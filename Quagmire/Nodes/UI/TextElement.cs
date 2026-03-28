c9283c7abc43ecde00bf5ec77ee9e6c81530e23f
Ravi <r.lebgue@gmail.com>
2026-03-28 14:10:37 -0500

    Text alignment

--- Quagmire/Nodes/UI/TextElement.cs
+++ Quagmire/Nodes/UI/TextElement.cs
@@ -4,11 +4,18 @@ namespace Quagmire.Nodes.UI;
 
 public class TextElement : UIElement
 {
+    public enum Alignment
+    {
+        Leading,
+        Center,
+        Trailing
+    }
     public Color Color = Color.White;
     public string Text = "";
     public int Size = 24;
     public Font? Font = null;
-    
+    public Alignment HorizontalAlignment = Alignment.Leading;
+    public Alignment VerticalAlignment = Alignment.Leading;
     
     
     public TextElement()
@@ -20,6 +27,48 @@ public class TextElement : UIElement
     {
         if (Font == null)
             return;
-        Draw.Text(Text, Font, Size, ScreenRect.Position, Color, (int)ScreenRect.Size.X);
+        var size = Font.Measure(Text, Size, (int)ScreenRect.Size.X);
+        
+        var position = ScreenRect.Position;
+
+        switch (HorizontalAlignment)
+        {
+            case Alignment.Leading:
+            {
+                break;
+            }
+            case Alignment.Center:
+            {
+                position.X += (ScreenRect.Size.X - size.X) / 2;
+                break;
+            }
+            case Alignment.Trailing:
+            {
+                position.X += ScreenRect.Size.X - size.X;
+                break;
+            }
+        }
+
+        switch (VerticalAlignment)
+        {
+            case Alignment.Leading:
+            {
+                break;
+            }
+            case Alignment.Center:
+            {
+                position.Y += (ScreenRect.Size.Y - size.Y) / 2;
+                break;
+            }
+            case Alignment.Trailing:
+            {
+                position.Y += ScreenRect.Size.Y - size.Y;
+                break;
+            }
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+        
+        Draw.Text(Text, Font, Size, position, Color, (int)ScreenRect.Size.X);
     }
 }
